97cec8a18e7fb24c1d681f94284758b478364e2b
Genady Perchenko <gperchenko@gmail.com>
2018-03-04 22:54:38 -0500

    initial commit

---
+++ MyCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyCodeCamp.Data;
+using MyCodeCamp.Data.Entities;
+
+namespace MyCodeCamp.Controllers
+{
+	[Route(("api/[controller]"))]
+	public class CampsController : Controller
+	{
+		private ICampRepository _repo;
+
+		public CampsController(ICampRepository repo)
+		{
+			_repo = repo;
+		}
+
+		[HttpGet("")]
+		public IActionResult Get()
+		{
+			var camps = _repo.GetAllCamps();
+			return Ok(camps);
+		}
+
+		//[HttpGet("{id}", Name ="CampGet" )]
+		//public IActionResult Get(int id, bool includeSpeakers = false)
+		//{
+		//	try
+		//	{
+		//		var camp = includeSpeakers ? _repo.GetCampWithSpeakers(id) : _repo.GetCamp(id);
+
+		//		if (camp == null) return NotFound($"Camp {id} was not found");
+
+		//		return Ok(camp);
+		//	}
+		//	catch (Exception e)
+		//	{	
+		//	}
+
+		//	return BadRequest();
+		//}
+
+		//[HttpPost("")]
+		//public async Task<IActionResult> Post([FromBody] Camp model)
+		//{
+		//	try
+		//	{
+		//		_repo.Add(model);
+		//		if (await _repo.SaveAllAsync())
+		//		{
+		//			var newUri = Url.Link("CampGet", new {id = model.Id});
+		//			return Created(newUri, model);
+		//		}
+		//	}
+		//	catch (Exception e)
+		//	{
+		//	}
+
+		//	return BadRequest();
+		//}
+	}
+}
--- MyCodeCamp/Startup.cs
+++ MyCodeCamp/Startup.cs
@@ -1,28 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using MyCodeCamp.Data;
 
 namespace MyCodeCamp
 {
     public class Startup
     {
+
+        private IConfiguration _config;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _config = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_config);
+            services.AddDbContext<CampContext>(ServiceLifetime.Scoped);
+            services.AddScoped<ICampRepository, CampRepository>();
+
+            // add framework 
             services.AddMvc();
         }
 
